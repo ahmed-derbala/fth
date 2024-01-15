d708461b363e16070bc913b15d022078d4f72ca5
Ahmed Derbala <ahmed.derbala@esprit.tn>
2024-01-15 22:30:50 +0100

    feat: fix conatct-us page

--- src/components/contact-us/contact-us.render.js
+++ src/components/contact-us/contact-us.render.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const config = require(`../../config`)
+const { mainData } = require('../index/index.service')
 
 router.get('/', function (req, res, next) {
 	const { NODE_ENV, app } = config
 	//return res.status(200).json({ NODE_ENV, app })
-	res.render('contact-us/views/contact-us', { title: config.app.name })
+	res.render('contact-us/views/contact-us', { title: config.app.name, mainData: mainData() })
 })
 
 module.exports = router
